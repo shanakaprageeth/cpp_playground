9d1de3c2bb3a6719fc943b7677c33b8910cbbe50
Shanaka Prageeth <Shanakaprageeth@gmail.com>
2025-03-15 01:12:20 +0000

    [refactor}:

--- src/socket/SocketServer.cpp
+++ src/socket/SocketServer.cpp
@@ -1,14 +1,13 @@
 // Copyright 2019 Shanaka Prageeth
 
-#include "socket/SocketServer.hpp"
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
 #include <cstdio>
+#include <iostream>
 #include <string>
+#include "socket/SocketServer.hpp"
 
 using std::cout;
 using std::endl;
@@ -23,65 +22,74 @@ SocketServer::SocketServer() {
 }
 
 int SocketServer::initSocket() {
+    int ret_val = 0;  // Initialize return value
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == 0) {
+        ret_val = -1;
         perror("Socket failed");
-        return -1;
     }
-    return 0;
+    return ret_val;  // Single return statement
 }
 
 int SocketServer::setSocket() {
+    int ret_val = 0;  // Initialize return value
     socket_set = setsockopt(server_fd,
                             SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                             &opt, sizeof(opt));
-    if (socket_set) {
+    if (socket_set != 0) {
+        ret_val = -1;
         perror("Setsockopt failed");
-        return -1;
     }
-    return 0;
+    return ret_val;  // Single return statement
 }
 
 int SocketServer::bindAndListen() {
+    int ret_val = 0;  // Initialize return value
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
     if (bind(server_fd,
-            (struct sockaddr *)&address, sizeof(address)) < 0) {
+            reinterpret_cast<struct sockaddr *>(&address),
+            sizeof(address)) < 0) {
+        ret_val = -1;
         perror("Bind failed");
-        return -1;
-    }
-    if (listen(server_fd, 3) < 0) {
+    } else if (listen(server_fd, 3) < 0) {
+        ret_val = -1;
         perror("Listen failed");
-        return -1;
     }
-    return 0;
+    return ret_val;  // Single return statement
 }
 
 int SocketServer::acceptClients() {
-    new_socket = accept(server_fd, (struct sockaddr *)&address,
+    int ret_val = 0;  // Initialize return value
+    new_socket = accept(server_fd,
+                        reinterpret_cast<struct sockaddr *>(&address),
                         reinterpret_cast<socklen_t *>(&addrlen));
     if (new_socket < 0) {
+        ret_val = -1;
         perror("Accept failed");
-        return -1;
+    } else {
+        accepted_sockets.push_back(new_socket);
+        ret_val = new_socket;
     }
-    accepted_sockets.push_back(new_socket);
-    return new_socket;
+    return ret_val;  // Single return statement
 }
 
 int SocketServer::readAndSend(const char *rcvMsg, const char *sendMsg) {
+    int ret_val = 0;  // Initialize return value
     char buffer[1024] = {0};
     int valread = read(new_socket, buffer, 1024);
     if (valread < 0) {
+        ret_val = -1;
         perror("Read failed");
-        return -1;
-    }
-    cout << "Received: " << buffer << endl;
+    } else {
+        cout << "Received: " << buffer << endl;
 
-    if (send(new_socket, sendMsg, strlen(sendMsg), 0) < 0) {
-        perror("Send failed");
-        return -1;
+        if (send(new_socket, sendMsg, strlen(sendMsg), 0) < 0) {
+            ret_val = -1;
+            perror("Send failed");
+        }
     }
-    return 0;
+    return ret_val;  // Single return statement
 }
